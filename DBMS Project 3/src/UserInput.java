60caf8f35daf83a7724c8b8ae8328caef57d317c
Om Jain <omjain2606@gmail.com>
2023-11-08 23:40:27 -0500

    Added UserInput class

---
+++ DBMS Project 3/src/UserInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class UserInput {
+    private final static Scanner sc = new Scanner(System.in);
+    public static String getString(String text){
+        System.out.print(text + ": ");
+        return sc.nextLine();
+    }
+    public static int getInt(String text){
+        System.out.print(text + ": ");
+        return sc.nextInt();
+    }
+
+    @Override
+    protected void finalize(){
+        try{
+            sc.close();
+        } catch(Exception e){
+            System.out.println("Error in destructor: " + e.getMessage());
+        }
+    }
+}
